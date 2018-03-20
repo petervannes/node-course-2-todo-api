98252a4074c86074ee4ef15569c78a446859e0f7
Peter van Nes <peter@reddipped.com>
2018-03-20 21:07:47 +0100

    added delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -19,16 +19,16 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
 
   const db = client.db('TodoApp');
 
-  // db.collection('Todos').insertOne({
-  //   text: 'hi!',
-  //   text2: 'mum.'
-  // }, (error, result) => {
-  //   if (error) {
-  //     return console('Unable to insert todo', err)
-  //   }
-  //
-  //   console.log(JSON.stringify(result.ops, undefined, 2))
-  // })
+  db.collection('Todos').insertOne({
+    text: 'hi!',
+    text2: 'mum.'
+  }, (error, result) => {
+    if (error) {
+      return console('Unable to insert todo', err)
+    }
+
+    console.log(JSON.stringify(result.ops, undefined, 2))
+  })
 
   // db.collection('Users').insertOne({
   //   name: 'Peter',
