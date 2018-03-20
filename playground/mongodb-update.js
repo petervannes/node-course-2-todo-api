fadbedec31fee9b20d1b9f2110876d8dd57185dc
Peter van Nes <peter@reddipped.com>
2018-03-20 21:27:18 +0100

    add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {
+  MongoClient, ObjectID
+} = require('mongodb');
+
+
+var obj = new ObjectID();
+
+console.log(obj);
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (error, client) => {
+
+  if (error) {
+    return console.log('Unable to connect to MongoDB server.')
+  }
+
+  console.log('Connected to MongoDB server.');
+
+  const db = client.db('TodoApp');
+
+
+
+  db.collection('Users').findOneAndUpdate({
+    name: 'Jan'
+  }, {
+    $inc: {
+      age: 1
+    },
+    $set: {
+      location: 'somewhere'
+    }
+  }, {
+    returnOriginal: false
+
+  }).then((result) => {
+    console.log(result);
+  })
+
+})
+
+//client.close();
